4c6d68d1d35ebe7abbe8afe74871447cc5faf008
Serhii Chervonyi <serhii.chervonyi@dev-pro.net>
2018-10-14 19:31:37 +0300

    Update

--- reflectHow/constructor.2.ts
+++ reflectHow/constructor.2.ts
@@ -1,24 +1,24 @@
 import 'reflect-metadata';
 
-function logParamTypes<T extends {new(...args:any[]):{}}>(constructor: T) {
-  const types = Reflect.getMetadata('design:paramtypes', constructor);
-  const s = types.map(a => a.name).join();
-  console.log(`param types: ${s}`);
+function LogParamTypes<T extends { new(...args: any[]): {} }>(constructor: T) {
+    const types = Reflect.getMetadata('design:paramtypes', constructor);
+    const s = types.map(a => a.name).join();
+    console.log(`param types: ${s}`);
 }
 
-class Foo {}
-interface IFoo {}
+class Foo { }
+interface IFoo { }
 
-@logParamTypes
+@LogParamTypes
 class Demo {
-  constructor(
-    param1 : string,
-    param2 : number,
-    param3 : Foo,
-    param4 : { test : string },
-    param5 : IFoo,
-    param6 : Function,
-    param7 : (a : number) => void,
-  ) {
-  }
+    constructor(
+        param1: string,
+        param2: number,
+        param3: Foo,
+        param4: { test: string },
+        param5: IFoo,
+        param6: Function,
+        param7: (a: number) => void,
+    ) {
+    }
 }
